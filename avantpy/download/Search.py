94b0279a629e64b969364b731cc24b98b6a1429e
kalishcode <mateusengenharia1@gmail.com>
2023-04-25 17:56:08 -0300

    documented memory

--- avantpy/download/Search.py
+++ avantpy/download/Search.py
@@ -158,7 +158,7 @@ class Search:
         return searchQuery
 
     def search(self):
-        """ Searches for documents in Elasticsearch using the given query."""
+        """Searches for documents in Elasticsearch using the given query."""
         self.log.info('Searching {} in {}'.format(self.index, self.url))
         try:
             self.response = requests.post(self.url+self.api_custom,
@@ -235,11 +235,7 @@ class Search:
         return newData
 
     def memory_search(self) -> List[Any]:
-        """Searches for the stored data in memory by the given key.
-
-        Returns:
-            List[Any]: The data retrieved from the memory storage. If the value is a list, it is extended to the
-        """
+        """Searches for the stored data in memory by the given key."""
         payload = {
             'key': self.key
         }
